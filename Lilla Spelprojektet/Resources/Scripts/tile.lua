18a1f15ef240f02096e6dcf33cee38c3be814f26
Olle@School <Olle@DESKTOP-UQFUSF3>
2018-04-05 13:00:16 +0200

    Small changes

--- Lilla Spelprojektet/Resources/Scripts/tile.lua
+++ Lilla Spelprojektet/Resources/Scripts/tile.lua
@@ -1,4 +1,20 @@
+local  = {}
+Tile.__index = Tile
 
-tilePosition x,y
+setmetatable(Tile, {
+  __index = Object, -- this is what makes the inheritance work
+  __call = function (cls, ...)
+    local self = setmetatable({}, cls)
+    self:_init(...)
+    return self
+  end,
+})
 
-function getPosition
+function Tile:_init(init1, init2)
+  BaseClass._init(self, init1) -- call the base class constructor
+  self.value2 = init2
+end
+
+function Tile:get_value()
+  return self.value + self.value2
+end
