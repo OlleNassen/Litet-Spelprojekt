60d4b85e5d803b468ec39d15d9c297029e5c62e0
Edvard Leiborn <edvardleiborn@telia.com>
2018-04-09 16:20:01 +0200

    Setup fil fixad

---
+++ Lilla Spelprojektet/Resources/Scripts/Setup/Setup.lua
@@ -0,0 +1,9 @@
+require ("/Resources/Scripts/Setup/Input")
+
+setResolution(1280, 720)
+setFramerate(60)
+
+bindKeyboard("moveRight", keyboard.D, keyboard.A)
+bindKeyboard("jump", keyboard.Space, keyboard.None)
+
+push("Resources/Scripts/InputTest.lua")
