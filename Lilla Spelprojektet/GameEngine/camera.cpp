f2c02dc350a67e0cab95187fe4f7f308a7e8b764
Edvard <Edvard@DESKTOP-KN74GAC>
2018-04-09 11:14:08 +0200

    camera

--- Lilla Spelprojektet/GameEngine/camera.cpp
+++ Lilla Spelprojektet/GameEngine/camera.cpp
@@ -1,11 +1,193 @@
 #include "camera.hpp"
+#include <cmath>
 
-Camera::Camera(GLuint width, GLuint height)
+Camera::Camera() :
+	center(),
+	size(),
+	rotation(0),
+	viewport(0, 0, 1, 1),
+	transformUpdated(false),
+	invTransformUpdated(false)
+{
+	reset(FloatRect(0, 0, 1000, 1000));
+}
+
+Camera::Camera(const FloatRect& rectangle) :
+	center(),
+	size(),
+	rotation(0),
+	viewport(0, 0, 1, 1),
+	transformUpdated(false),
+	invTransformUpdated(false)
+{
+	reset(rectangle);
+}
+
+Camera::Camera(const sf::Vector2f& center, const sf::Vector2f& size) :
+	center(center),
+	size(size),
+	rotation(0),
+	viewport(0, 0, 1, 1),
+	transformUpdated(false),
+	invTransformUpdated(false)
 {
 
 }
 
-Camera::~Camera()
+void Camera::setCenter(float x, float y)
 {
+	center.x = x;
+	center.y = y;
+
+	transformUpdated = false;
+	invTransformUpdated = false;
+}
+
+void Camera::setCenter(const sf::Vector2f& center)
+{
+	setCenter(center.x, center.y);
+}
+
+
+void Camera::setSize(float width, float height)
+{
+	size.x = width;
+	size.y = height;
+
+	transformUpdated = false;
+	invTransformUpdated = false;
+}
+
+void Camera::setSize(const sf::Vector2f& size)
+{
+	setSize(size.x, size.y);
+}
+
+void Camera::setRotation(float angle)
+{
+	rotation = static_cast<float>(fmod(angle, 360));
+	if (rotation < 0)
+		rotation += 360.f;
+
+	transformUpdated = false;
+	invTransformUpdated = false;
+}
+
+void Camera::setViewport(const FloatRect& viewport)
+{
+	this->viewport = viewport;
+}
+
+
+void Camera::reset(const FloatRect& rectangle)
+{
+	center.x = rectangle.left + rectangle.width / 2.f;
+	center.y = rectangle.top + rectangle.height / 2.f;
+	size.x = rectangle.width;
+	size.y = rectangle.height;
+	rotation = 0;
+
+	transformUpdated = false;
+	invTransformUpdated = false;
+}
+
+
+////////////////////////////////////////////////////////////
+const sf::Vector2f& Camera::getCenter() const
+{
+	return center;
+}
+
+
+////////////////////////////////////////////////////////////
+const sf::Vector2f& Camera::getSize() const
+{
+	return size;
+}
+
+
+////////////////////////////////////////////////////////////
+float Camera::getRotation() const
+{
+	return rotation;
+}
+
+
+////////////////////////////////////////////////////////////
+const FloatRect& Camera::getViewport() const
+{
+	return viewport;
+}
+
+
+////////////////////////////////////////////////////////////
+void Camera::move(float offsetX, float offsetY)
+{
+	setCenter(center.x + offsetX, center.y + offsetY);
+}
+
+
+////////////////////////////////////////////////////////////
+void Camera::move(const sf::Vector2f& offset)
+{
+	setCenter(center + offset);
+}
+
+
+////////////////////////////////////////////////////////////
+void Camera::rotate(float angle)
+{
+	setRotation(rotation + angle);
+}
+
+
+////////////////////////////////////////////////////////////
+void Camera::zoom(float factor)
+{
+	setSize(size.x * factor, size.y * factor);
+}
+
+
+////////////////////////////////////////////////////////////
+const glm::mat3& Camera::getTransform() const
+{
+	// Recompute the matrix if needed
+	if (!transformUpdated)
+	{
+		// Rotation components
+		float angle = rotation * 3.141592654f / 180.f;
+		float cosine = static_cast<float>(std::cos(angle));
+		float sine = static_cast<float>(std::sin(angle));
+		float tx = -center.x * cosine - center.y * sine + center.x;
+		float ty = center.x * sine - center.y * cosine + center.y;
+
+		// Projection components
+		float a = 2.f / size.x;
+		float b = -2.f / size.y;
+		float c = -a * center.x;
+		float d = -b * center.y;
+
+		// Rebuild the projection matrix
+		transform = glm::mat3(a * cosine, a * sine, a * tx + c,
+			-b * sine, b * cosine, b * ty + d,
+			0.f, 0.f, 1.f);
+		transformUpdated = true;
+	}
+
+	return transform;
+}
+
+
+////////////////////////////////////////////////////////////
+const glm::mat3& Camera::getInverseTransform() const
+{
+	// Recompute the matrix if needed
+	if (!invTransformUpdated)
+	{
+		inverseTransform = glm::inverse(getTransform());
+		invTransformUpdated = true;
+	}
+
+	return inverseTransform;
 }
 
--- Lilla Spelprojektet/GameEngine/camera.hpp
+++ Lilla Spelprojektet/GameEngine/camera.hpp
@@ -1,15 +1,63 @@
-#pragma once
-#include "../libs.h"
+#ifndef CAMERA_HPP
+#define CAMERA_HPP
+
+#include <glm/glm.hpp>
+#include <SFML/System/Vector2.hpp>
+
+struct FloatRect
+{
+	FloatRect(float left, float top, float width, float height)
+	{
+		this->left = left;
+		this->top = top;
+		this->width = width;
+		this->height = height;
+	}
+	
+	float left;
+	float top;
+	float width;
+	float height;
+};
+
 
 class Camera
 {
-private:
-
-	GLfloat windowWidth;
-	GLfloat windowHeight;
-
 public:
-	Camera(GLuint width, GLuint height);
-	~Camera();
 
+	Camera();
+
+	explicit Camera(const FloatRect& rectangle);
+	Camera(const sf::Vector2f& center, const sf::Vector2f& size);
+
+	void setCenter(float x, float y);	
+	void setCenter(const sf::Vector2f& center);
+	void setSize(float width, float height);
+	void setSize(const sf::Vector2f& size);
+	void setRotation(float angle);
+	void setViewport(const FloatRect& Cameraport);
+	void reset(const FloatRect& rectangle);
+	const sf::Vector2f& getCenter() const;
+	const sf::Vector2f& getSize() const;
+	float getRotation() const;
+	const FloatRect& getViewport() const;
+	void move(float offsetX, float offsetY);
+	void move(const sf::Vector2f& offset);
+	void rotate(float angle);
+	void zoom(float factor);
+	const glm::mat3& getTransform() const;
+	const glm::mat3& getInverseTransform() const;
+
+private:
+	sf::Vector2f center;              ///< Center of the Camera, in scene coordinates
+	sf::Vector2f size;                ///< Size of the Camera, in scene coordinates
+	float rotation;            ///< Angle of rotation of the Camera rectangle, in degrees
+	FloatRect viewport;            ///< Cameraport rectangle, expressed as a factor of the render-target's size
+	mutable glm::mat3 transform;           ///< Precomputed projection glm::mat3 corresponding to the Camera
+	mutable glm::mat3 inverseTransform;    ///< Precomputed inverse projection glm::mat3 corresponding to the Camera
+	mutable bool transformUpdated;    ///< Internal state telling if the glm::mat3 needs to be updated
+	mutable bool invTransformUpdated; ///< Internal state telling if the inverse glm::mat3 needs to be updated
 };
+
+
+#endif // CAMERA_HPP
