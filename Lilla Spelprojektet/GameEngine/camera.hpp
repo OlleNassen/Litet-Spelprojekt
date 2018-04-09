73d55152920e2b9181ef0d783415eaea96cfd922
Olle@School <Olle@DESKTOP-UQFUSF3>
2018-04-09 12:16:46 +0200

    fixed bug

--- Lilla Spelprojektet/GameEngine/camera.hpp
+++ Lilla Spelprojektet/GameEngine/camera.hpp
@@ -49,9 +49,9 @@ private:
 	sf::Vector2f size;                ///< Size of the Camera, in scene coordinates
 	float rotation;            ///< Angle of rotation of the Camera rectangle, in degrees
 	FloatRect viewport;           
-	glm::vec3 position;
-	glm::vec3 front;
-	glm::vec3 up;
+	//glm::vec3 position;
+	//glm::vec3 front;
+	//glm::vec3 up;
 
 };
 
