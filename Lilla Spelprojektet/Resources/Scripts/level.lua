5e8a6d725df1e290f73ef84a5d01aa7d8b1c9286
VINCENT MÅNSSON <vimc15@student.bth.se>
2018-04-10 10:23:33 +0200

    Changed in Tile and level lua

--- Lilla Spelprojektet/Resources/Scripts/level.lua
+++ Lilla Spelprojektet/Resources/Scripts/level.lua
@@ -19,3 +19,36 @@ for x = 0, 100 do
 		position = {xVal, yVal}
 		print(tileMap[x])
 end
+
+mapSizeX = 0
+mapSizeY = 0
+tileMap = {}
+function createTileMap(sizeX, sizeY)
+	mapSizeX = sizeX
+	mapSizeY = sizeY
+	for y = 0, sizeY do
+		for x = 0, sizeX do
+			tempTile = createTile(x, y, 0)
+			print(tempTile)
+			tileMap[sizeX * y + x] = tempTile
+		end
+	end
+end
+
+function createTile(x, y, type)
+	position = {x, y}
+	tempTile = Tile:create(position, type)
+	return tempTile
+end
+
+function loadFromFile(file)
+	local file = io.open(file)
+	index = 0
+	if file then
+		for line in file:lines() do
+			local posX, posY, type = unpack(line:split(","))
+			tileMap[index] = createTile(posX, posY, type)
+			index = index + 1
+		end
+	end
+end
--- Lilla Spelprojektet/Resources/Scripts/tile.lua
+++ Lilla Spelprojektet/Resources/Scripts/tile.lua
@@ -5,7 +5,7 @@
 Tile = {}
 Tile.__index = Tile
 
-function Tile:create(position)
+function Tile:create(position, type)
    local tile = {}
 
    tile.x = position.x
@@ -14,6 +14,9 @@ function Tile:create(position)
    --Tile size:
 	tile.width = 48
 	tile.height = 48
+
+	--Tile type:
+	tile.type = type;
 
    setmetatable(tile,Tile)
 
@@ -30,4 +33,8 @@ end
 
 function Tile:print()
 	print(self.width, self.height)
+end
+
+function Tile:setTile(type)
+	self.type = type
 end
