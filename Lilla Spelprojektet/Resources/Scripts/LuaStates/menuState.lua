c62cdc23adec730b3fa02040f46decac9e2c0d33
Olle@School <Olle@DESKTOP-UQFUSF3>
2018-04-16 15:01:38 +0200

    LA MENU

--- Lilla Spelprojektet/Resources/Scripts/LuaStates/menuState.lua
+++ Lilla Spelprojektet/Resources/Scripts/LuaStates/menuState.lua
@@ -8,6 +8,24 @@ local cam = newSprite(0, 0, 0, s.texture)
 s.sprite = newSprite(1280,720, 0, s.texture)
 s:setPosition(-1280/2, -720/2)
 
+
+local play = Entity:create() 
+play.texture = newTexture("Resources/Sprites/play.png")
+play.sprite = newSprite(800,100, 0, play.texture)
+play:setPosition(-400,-300)
+
+local editor = Entity:create() 
+editor.texture = newTexture("Resources/Sprites/editor.png")
+editor.sprite = newSprite(800,100, 0, editor.texture)
+editor:setPosition(-400,100)
+
+local exit = Entity:create() 
+exit.texture = newTexture("Resources/Sprites/quit.png")
+exit.sprite = newSprite(800,100, 0, exit.texture)
+exit:setPosition(-400,210)
+
+
+
 function moveUp(direction, deltaTime)
 	--p:move(direction * p.speed * deltaTime, 0)
 end
@@ -25,5 +43,4 @@ function select(direction, deltaTime)
 end
 
 function update(deltaTime)
-	print ("Press enter to start!")
 end
