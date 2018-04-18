5b7652bf2c38e5baed91c1f471e1ddbf593d8926
Suraj Sharma <suraj-sharma@live.com>
2018-04-18 12:30:22 +0200

    Worked on particles

--- Lilla Spelprojektet/GameEngine/particle_emitter.hpp
+++ Lilla Spelprojektet/GameEngine/particle_emitter.hpp
@@ -8,15 +8,28 @@ class ParticleEmitter
 {
 private:
 	std::vector<glm::vec2> particle_positions;
+	std::vector<float> particle_lifetime;
 	Sprite* particle;
 
+	//Private functions
+
 public:
+	//Con & Des
 	ParticleEmitter(
 		Shader* shader, 
 		Texture2D* texture, 
 		Texture2D* normalMap = nullptr, 
-		const glm::vec2& size = glm::vec2(48, 48));
+		const glm::vec2& size = glm::vec2(10, 10));
 
 	virtual ~ParticleEmitter();
+
+	//Functions
+	void update();
+	void render(const glm::mat4& view, const glm::mat4& projection);
+
+	//Accessors
+
+	//Modifiers
+
 };
 
