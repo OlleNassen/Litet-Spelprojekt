2db7228c4c109d6bfdae27c8aedeaefb228f2069
Suraj Sharma <suraj-sharma@live.com>
2018-04-19 11:20:22 +0200

    worked on particle system

--- Lilla Spelprojektet/GameEngine/particle_emitter.hpp
+++ Lilla Spelprojektet/GameEngine/particle_emitter.hpp
@@ -14,36 +14,48 @@ private:
 	class Particle
 	{
 	public:
-		Particle(glm::vec2 velocity, float lifeTimeMax)
-		{
-			this->velocity = velocity;
-			this->lifeTimeMax = lifeTimeMax;
-			this->lifeTime = this->lifeTimeMax;
-		}
 
 		glm::vec2 velocity;
 		glm::vec2 position;
-		float lifeTimeMax;
 		float lifeTime;
+
+		Particle(const glm::vec2& velocity, const float& lifeTime)
+		{
+			this->velocity = velocity;
+			this->lifeTime = lifeTime;
+		}
+
+		void update(const float& dt)
+		{
+			if (this->lifeTime > 0.f)
+			{
+				this->lifeTime -= 10 * dt;
+				this->position += velocity * dt;
+			}
+		}
 	};
 
 	std::vector<Particle> particles;
 	Sprite* particle;
+	glm::vec2 velocity;
+	float lifeTime;
 
 	//Private functions
 
 public:
 	//Con & Des
 	ParticleEmitter(
-		Shader* shader, 
-		Texture2D* texture, 
-		Texture2D* normalMap = nullptr, 
-		const glm::vec2& size = glm::vec2(10, 10));
+		Shader* shader,
+		Texture2D* texture,
+		Texture2D* normalMap = nullptr,
+		const glm::vec2& size = glm::vec2(0, 0),
+		const glm::vec2& velocity = glm::vec2(0.f, 0.f),
+		const float& lifeTime = 0.f);
 
 	virtual ~ParticleEmitter();
 
 	//Functions
-	void update();
+	void update(const float& dt);
 	void render(const glm::mat4& view, const glm::mat4& projection);
 
 	//Accessors
