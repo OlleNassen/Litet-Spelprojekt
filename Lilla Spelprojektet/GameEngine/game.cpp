8348b864deb01eee017e22eb1f33cd32dbca7be2
Edvard <Edvard@DESKTOP-KN74GAC>
2018-05-02 11:27:17 +0200

    Revert "added binding for getCameraPosition"
    This reverts commit b745d16a297e87d0756a825fb493ca47d68dc431.

--- Lilla Spelprojektet/GameEngine/game.cpp
+++ Lilla Spelprojektet/GameEngine/game.cpp
@@ -214,9 +214,6 @@ void Game::addLuaLibraries(lua_State* luaState)
 	lua_pushcfunction(luaState, clear);
 	lua_setglobal(luaState, "clear");
 
-	lua_pushcfunction(luaState, getCameraPosition);
-	lua_setglobal(luaState, "getCameraPosition");
-
 	eventSystem.addLuaRebind(luaState);
 }
 
