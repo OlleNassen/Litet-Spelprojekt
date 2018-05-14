f6a6cbe02275a73036a59a44c36aec136afeb4fb
VINCENT MÅNSSON <vimc15@student.bth.se>
2018-05-14 12:37:31 +0200

    Fix

--- Lilla Spelprojektet/Resources/Scripts/LuaStates/edvardState.lua
+++ Lilla Spelprojektet/Resources/Scripts/LuaStates/edvardState.lua
@@ -233,6 +233,8 @@ end
 
 function update(deltaTime)
 	
+	checkUpgrades(deltaTime)
+
 	p:update(deltaTime)
 	s:setPosition(p.entity.x + mX, p.entity.y + mY)
 
@@ -267,3 +269,51 @@ function update(deltaTime)
 	end
 end
 
+function checkUpgrades(deltaTime)
+	if p.entity.hasPowerUp[1] == true then -- DASH UPGRADE
+	
+		if p.entity.collision_bottom == true then --Cant dash until on ground
+			p.canDash = true
+		end
+
+		if p.dashing == true then --Dashing
+			p.entity.hasGravity = false
+			p.canDash = false
+			if hasFoundPosition == false then
+				towardsX = s.x
+				towardsY = s.y
+				hasFoundPosition = true
+			end
+
+			local tempX = towardsX - p.entity.x 
+			local tempY = towardsY - p.entity.y 
+			local length = math.sqrt((tempX * tempX) + (tempY * tempY))
+			tempX = (tempX / length)
+			tempY = (tempY / length)
+	
+			p.entity.velocity.x = tempX * 2000
+			p.entity.velocity.y = tempY * 2000
+
+			print(p.entity.velocity.x)
+			print(p.entity.velocity.y)
+
+			--Dashing ends
+			if length < 30 or p.entity.collision_top == true or  p.entity.collision_left == true or p.entity.collision_right == true or p.entity.collision_bottom == true then
+				p.dashing = false
+				p.entity.hasGravity = true
+				hasFoundPosition = false
+				p.entity.velocity.x = p.entity.velocity.x / 5
+				p.entity.velocity.y = p.entity.velocity.y / 5
+			end
+		end
+	end
+	if p.entity.hasPowerUp[2] == true then -- SPEED UPGRADE
+		p.entity.maxSpeed.x = 800
+	end
+	if p.entity.hasPowerUp[3] == true then -- DOUBLE JUMP UPGRADE
+		p.maxNrOfJumps = 2
+	end
+	if p.entity.hasPowerUp[4] == true then -- HIGH JUMP UPGRADE
+		p.jumpPower = -1800
+	end
+end
