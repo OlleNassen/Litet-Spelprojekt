c905e74a214e2ab33970da00a69a70c0a0bcd082
Edvard <Edvard@DESKTOP-KN74GAC>
2018-05-16 14:54:06 +0200

    tiled template

---
+++ Lilla Spelprojektet/normal.tsx
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="normal" tilewidth="48" tileheight="48" tilecount="11" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="48" height="48" source="Resources/Sprites/brick_normal.png"/>
+ </tile>
+ <tile id="1">
+  <image width="48" height="48" source="Resources/Sprites/pyramid_normal.png"/>
+ </tile>
+ <tile id="2">
+  <image width="48" height="48" source="Resources/Sprites/iron_block_normal.png"/>
+ </tile>
+ <tile id="3">
+  <image width="48" height="48" source="Resources/Sprites/brickSolidMix_normal.png"/>
+ </tile>
+ <tile id="4">
+  <image width="48" height="48" source="Resources/Sprites/solidScrew_normal.png"/>
+ </tile>
+ <tile id="5">
+  <image width="48" height="48" source="Resources/Sprites/ironPillar_normal.png"/>
+ </tile>
+ <tile id="6">
+  <image width="48" height="48" source="Resources/Sprites/lockerBottom_normal.png"/>
+ </tile>
+ <tile id="7">
+  <image width="48" height="48" source="Resources/Sprites/lockerTop_normal.png"/>
+ </tile>
+ <tile id="8">
+  <image width="48" height="48" source="Resources/Sprites/lampCoord_normal.png"/>
+ </tile>
+ <tile id="9">
+  <image width="48" height="48" source="Resources/Sprites/durkplat_normal.png"/>
+ </tile>
+ <tile id="10">
+  <image width="48" height="48" source="Resources/Sprites/chain_normal.png"/>
+ </tile>
+</tileset>
